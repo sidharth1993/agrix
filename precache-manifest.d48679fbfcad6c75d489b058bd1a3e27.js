f94170c94de702fc4d56fc0b66c1db5c0e765e13
Sai Sidharth Giridharan <gsaisidharth@gmail.com>
2019-11-21 01:18:02 +0530

    Updates

---
+++ precache-manifest.d48679fbfcad6c75d489b058bd1a3e27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945318e6c755b47d29864ed56830094e",
+    "url": "/agrix/index.html"
+  },
+  {
+    "revision": "886234ba6999c175962f",
+    "url": "/agrix/static/css/2.3c21dd12.chunk.css"
+  },
+  {
+    "revision": "0160e4ddf9cbf0c6582c",
+    "url": "/agrix/static/css/main.72669e63.chunk.css"
+  },
+  {
+    "revision": "886234ba6999c175962f",
+    "url": "/agrix/static/js/2.c65d369e.chunk.js"
+  },
+  {
+    "revision": "0160e4ddf9cbf0c6582c",
+    "url": "/agrix/static/js/main.fae8f87f.chunk.js"
+  },
+  {
+    "revision": "bc77d441280a428847dd",
+    "url": "/agrix/static/js/runtime-main.90a31a6c.js"
+  },
+  {
+    "revision": "d2193e82872b98ab8889d7f83a3e3d1f",
+    "url": "/agrix/static/media/logo.d2193e82.png"
+  },
+  {
+    "revision": "523b36afd9ea5cad47c8436b57226069",
+    "url": "/agrix/static/media/result.523b36af.pdf"
+  }
+]);
