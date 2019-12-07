0860fad4dd07f903aa2cc6f12fc52c4215442b4c
manoaravindhan <man.oz@live.in>
2019-12-07 18:05:21 +0530

    initial commit for leaflet.js <Major change>

---
+++ src/components/map/indexMap.js
@@ -0,0 +1,232 @@
+import React, { useState, useEffect } from "react";
+import OlMap from "ol/Map";
+import OlView from "ol/View";
+import OlLayerTile from "ol/layer/Tile";
+import GeoJSON from 'ol/format/GeoJSON';
+import OlSourceOSM from "ol/source/OSM";
+import Zoom from 'ol/control/Zoom';
+import { defaults } from 'ol/interaction'
+import Draw from 'ol/interaction/Draw';
+import { doubleClick } from 'ol/events/condition';
+import Select from 'ol/interaction/Select';
+import { Fill, Stroke, Style, Text} from 'ol/style';
+import windowDimensions from 'react-window-dimensions';
+import axios from 'axios';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import './styles/map.scss';
+import MapControl from './MapControls';
+import 'ol/ol.css';
+import { message } from 'antd';
+
+const center = [0, 0];
+const { REACT_APP_DOMAIN: domain, REACT_APP_LOGIN_PORT: port } = process.env;
+
+const styleBorder = feature => new Style({
+  stroke: new Stroke({
+    color: '#0099FF',
+    width: 1
+  }),
+  fill: new Fill({
+    color: 'rgba(0, 0, 0, 0)'
+  })
+});
+
+const styleDt = feature => new Style({
+  stroke: new Stroke({
+    color: '#A03582',
+    width: 2
+  }),
+  fill: new Fill({
+    color: 'rgba(255, 255, 0, 0.1)'
+  }),
+  text: new Text({
+    text: feature.values_.BLKNAME,
+    fill: new Fill({
+      color: '#f56a00'
+    })
+  })
+});
+let olmap = null;
+const defaultZoom = 1;
+const defaultShowSubmit = false;
+const defaultLevel = -1;
+let draw = null;
+const drawSource = new VectorSource({ wrapX: false });
+const drawVector = new VectorLayer({ source: drawSource });
+const drawNewPoly = { source: drawSource, type: 'Polygon'};
+
+const Map = ({height,width,logged}) => {
+
+  const [zoom, setZoom] = useState(defaultZoom);
+  const [showSubmit, setShowSubmit] = useState(defaultShowSubmit);
+  //const [level, setLevel] = useState(defaultLevel);
+  let level = defaultLevel;
+
+  const handleSubmit = (showSubmit) => {
+    setShowSubmit(showSubmit);
+  }
+
+  const toggleEdit = (edit) => {
+    if (edit) {
+        olmap.getLayers().array_[2].getSource().clear();
+        draw = new Draw(drawNewPoly);
+        draw.on('drawend', () => { setShowSubmit(true) });
+        olmap.addInteraction(draw);
+    } else {
+        olmap && draw && olmap.removeInteraction(draw);
+        draw && draw.removeEventListener('drawend');
+        draw = null;
+    }
+}
+
+  const clearDraw = () => {
+    toggleEdit(false);
+    olmap.getLayers().array_[2].getSource().clear();
+  }
+
+  const fitToExtent = feature => {
+    olmap.getView().fit(feature.getGeometry().getExtent(), { duration: 2000 });
+  }
+
+  const selectArea = (source, id) => {
+    let url = `https://agrix-api.herokuapp.com/server/api/division?level=${level}`;
+    if (level === 1)
+      url += `&blockId=${id}`;
+    if (level === 0 || (level === 1 && id)) {
+      let hide = message.loading('Loading Map', 0);
+      axios.get(url).then(res => {
+        if (!res.data.status)
+          return;
+        let boundarySource = new VectorSource({
+          features: (new GeoJSON({
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+          })).readFeatures(res.data.data)
+        });
+        let layer;
+        if (level === 0) {
+          layer = olmap.getLayers().array_[1]
+          //setLevel(1);
+          level = 1;
+        } else if (level === 1) {
+          layer = olmap.getLayers().array_[3];
+        }
+        layer.setSource(boundarySource);
+        setTimeout(() => {
+          if (level === 1) {
+            fitToExtent(source.getFeatures().getArray()[0])
+          }
+        }, 500);
+        hide();
+      }, res => { if (level === 0) hide(); });
+    }
+  }
+
+  const select = new Select({
+    condition: doubleClick
+  })
+
+  const clearLayers = () => {
+    let layers = olmap.getLayers().array_;
+    layers.forEach(layer => {
+      layer.getSource().clear();
+    });
+  }
+
+  //componentDidMount
+  useEffect(()=>{
+    const boundarySource = new VectorSource();
+    const boundaryLayer = new VectorLayer({ source: boundarySource, style: f => styleBorder(f) });
+    const level1Source = new VectorSource();
+    const level1Layer = new VectorLayer({ source: level1Source, style: f => styleDt(f) });
+  
+    const view = new OlView({center, zoom: zoom});
+    const raster = new OlLayerTile({
+      source: new OlSourceOSM()
+    });
+    olmap = new OlMap({
+      interactions: defaults({
+        doubleClickZoom: false
+      }),
+      target: null,
+      layers: [
+        raster,
+        boundaryLayer,
+        drawVector,
+        level1Layer
+      ],
+      controls: [
+        new Zoom({
+          className: 'zoom'
+        })
+      ],
+      view: view
+    });
+    olmap.setTarget("draw-map");
+    olmap.on("moveend", () => {
+      let newZoom = olmap.getView().getZoom();
+      setZoom(newZoom);
+    });
+  },[]);
+
+  const effectComponentDidUpdate = ()=>{
+    if (logged && level === -1) {
+      let hide = message.loading('Loading Map', 0);
+      axios.get(`https://agrix-api.herokuapp.com/server/api/location/geojson`).then(res => {
+        if (!res.data.status) {
+          return;
+        }
+        //setLevel(0);
+        level = 0;
+        let boundarySource = new VectorSource({
+          features: (new GeoJSON({
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+          })).readFeatures(res.data.data)
+        });
+        olmap.getLayers().array_[1].setSource(boundarySource);
+        setTimeout(() => {
+          fitToExtent(olmap.getLayers().array_[1].getSource().getFeatures()[0])
+          hide();
+          message.info('Hint: Double click to load districts!')
+        }, 1000);
+        olmap.addInteraction(select);
+        select.on('select', e => {
+          try {
+              const id = e.selected[0].getProperties('values_').OBJECTID;
+              selectArea(e.target, id);
+              if (e.selected[0].values_.BLOCKS_)
+                fitToExtent(e.target.getFeatures().getArray()[0])              
+          } catch (e) {
+            console.log(e);
+          }
+        });
+      });
+    } else {
+        clearLayers();
+        olmap.removeInteraction(select);
+        select.removeEventListener('select');
+    } 
+  }
+
+  //componentDidUpdate
+  useEffect(()=>{
+    setTimeout(effectComponentDidUpdate,100);
+    },[logged]);
+  
+  //updateMap
+  return (
+    <>
+    {olmap && olmap.getView().setZoom(zoom)}
+      <div id="draw-map" style={{ width: "100%", height: `${height - 67}px` }}></div>
+      {logged && <MapControl
+        editAction={toggleEdit}
+        clearDraw={clearDraw}
+        handleSubmit={handleSubmit}
+        submit={showSubmit}/>}
+    </>
+  )
+}
+
+export default windowDimensions()(Map);
